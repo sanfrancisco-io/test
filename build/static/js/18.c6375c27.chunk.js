1c9fc9d7ca23124ec3d39819a451663dc968686d
aibek shamshiev <aibek@MacBook-Air-aibek.local>
2022-10-19 23:03:45 +0600

    final

---
+++ build/static/js/18.c6375c27.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[18],{505:function(e,s,n){"use strict";n.r(s);n(0);var t=n(4);s.default=function(){return Object(t.jsx)("div",{children:"Home component works!"})}}}]);
+//# sourceMappingURL=18.c6375c27.chunk.js.map
